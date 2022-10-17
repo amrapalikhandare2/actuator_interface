be599e44704b11d858eb3e94ac71f1a6bb664f45
Amrapali Khandare <amrapali.kh@rightbot.in>
2022-10-17 16:20:22 +0530

    underprogress

--- include/motor_controls.hpp
+++ include/motor_controls.hpp
@@ -61,6 +61,9 @@ class MotorControls{
     typedef struct velocity_cmd_ {
         double timeout;
         double velocity;
+        double max_vel; // position loop params
+        double accel;
+        double decel;
     }velocity_cmd_t;
 
     typedef struct position_cmd_ {
@@ -71,9 +74,33 @@ class MotorControls{
         double decel;
     }position_cmd_t;
 
-    bool motor_command(std::string motor_name, std::string command_type, position_cmd_t position, velocity_cmd_t velocity);
+    typedef union md_control_register_s_ {
+        struct control_s_
+        {
+            uint16_t switch_on:1;
+            uint16_t enable_voltage:1;
+            uint16_t quick_stop:1;
+            uint16_t enable_operation:1;
+            uint16_t new_setpoint:1;
+            uint16_t instantaneous_change_set:1;
+            uint16_t absolute_or_relative:1;                    // 0 is absolute, 1 is relative
+            uint16_t reset_fault:1;
+            uint16_t halt:1;
+            uint16_t reserved:7;
+        }control_s;
+
+        uint16_t control_word;
+    }md_control_register_u_t;
+
+    md_control_register_u_t md_control_register = {0};
+
+    // velocity_cmd_t velocity_cmd_element;
+    // position_cmd_t position_cmd_element;
+
+    bool motor_command(int motor_id, std::string command_type, position_cmd_t position_cmd_element, velocity_cmd_t velocity_cmd_element);
     
-    int set_vel_speed(float vel, uint16_t nodeid);
+    int set_vel_speed(int axis, uint16_t nodeid, float vel);
+    int set_relative_position(uint16_t node_id,uint32_t position);
 
     int motor_rpm_to_cps(float rpm);
     int motor_rps2_to_cps2(float rpss);
@@ -84,10 +111,8 @@ class MotorControls{
 
     int set_profile_deacc(uint16_t node_id,uint32_t deacc);
 
-    int set_driving_motor_position_mode_params(uint16_t node_id,float position_mode_acc, float position_mode_deacc, float position_mode_speed);
+    int set_driving_motor_position_mode_params(uint16_t node_id,double position_loop_acc, double position_loop_deacc, double position_loop_speed);
     
-
-
 };
 
 #endif
--- src/motor_controls.cpp
+++ src/motor_controls.cpp
@@ -20,14 +20,48 @@ int MotorControls::motor_rps2_to_cps2(float rpss) // RPS2 to 10 counts/sec2 conv
 	return m_cps2;
 }
 
-int MotorControls::set_vel_speed(float vel, uint16_t nodeid){
+int MotorControls::set_vel_speed(int axis, uint16_t nodeid, float vel){
     int err = 0;
-	const int32_t rpm = motor_rpm_to_cps(vel) ;
+	const int32_t rpm = motor_rpm_to_cps(axis*vel) ;
 	Socketcan_t target_vel[2] = {
 		{2, Switch_On_And_Enable_Operation},
 		{4, rpm}};
 
 	err = PDO_send(motor_sockets->motor_vel_pdo_fd, PDO_RX4_ID + nodeid, 2, target_vel);
+	return err;
+}
+
+int MotorControls::set_relative_position(uint16_t node_id,uint32_t position) {
+	int err = 0;
+
+	SDO_data d;
+	d.nodeid = node_id;
+	d.index = 0x607A;
+	d.subindex = 0x00;
+	d.data.size = 4;
+	d.data.data = position;//in milliseconds
+	err |= SDO_write(motor_sockets->motor_cfg_fd, &d);
+
+	md_control_register.control_s.switch_on = 1;
+	md_control_register.control_s.enable_voltage = 1;
+	md_control_register.control_s.quick_stop = 1;
+	md_control_register.control_s.enable_operation = 1;
+	md_control_register.control_s.absolute_or_relative = 0; // 1 for relative
+	md_control_register.control_s.new_setpoint = 0;
+
+	d.index = 0x6040;
+	d.subindex = 0x00;
+	d.data.size = 2;
+	d.data.data = md_control_register.control_word;
+	err |= SDO_write(motor_sockets->motor_cfg_fd, &d);
+	
+	md_control_register.control_s.absolute_or_relative = 1;
+	md_control_register.control_s.new_setpoint = 1;
+	d.index = 0x6040;
+	d.subindex = 0x00;
+	d.data.size = 2;
+	d.data.data = md_control_register.control_word;
+	err |= SDO_write(motor_sockets->motor_cfg_fd, &d);
 	return err;
 }
 
@@ -73,17 +107,20 @@ int MotorControls::set_profile_deacc(uint16_t node_id,uint32_t deacc) {
 	return err;
 }
 
-int MotorControls::set_driving_motor_position_mode_params(uint16_t node_id,float position_mode_acc, float position_mode_deacc, float position_mode_speed){
+int MotorControls::set_driving_motor_position_mode_params(uint16_t node_id, double position_loop_acc,  double position_loop_deacc,  double position_loop_speed){
 	int err = 0;
-    err |= set_profile_velocity(node_id,motor_rpm_to_cps(position_mode_speed) / DRIVING_MOTOR_REGISTER_MULTIPLIER); 
-	err |=set_profile_acc(node_id,motor_rps2_to_cps2(position_mode_acc));
-	err |=set_profile_deacc(node_id,motor_rps2_to_cps2(position_mode_deacc));
+    err |= set_profile_velocity(node_id,motor_rpm_to_cps(position_loop_speed) / DRIVING_MOTOR_REGISTER_MULTIPLIER); 
+	err |=set_profile_acc(node_id,motor_rps2_to_cps2(position_loop_acc));
+	err |=set_profile_deacc(node_id,motor_rps2_to_cps2(position_loop_deacc));
     return err;
 }
 
-bool MotorControls::motor_command(std::string motor_name, std::string command_type, position_cmd_t position, velocity_cmd_t velocity){
+bool MotorControls::motor_command(int motor_id, std::string command_type, position_cmd_t position_cmd_element, velocity_cmd_t velocity_cmd_element){
     
     if (command_type =="velocity"){
+        int axis=1;
+        set_driving_motor_position_mode_params(motor_id, velocity_cmd_element.accel, velocity_cmd_element.decel, velocity_cmd_element.max_vel);
+        set_vel_speed(axis, motor_id, velocity_cmd_element.velocity );
 
     }
 
